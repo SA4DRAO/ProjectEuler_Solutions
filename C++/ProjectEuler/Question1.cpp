a36e3402de81a621499c823c3b1956ba69b9016f
adrao <15830398+Adrao123@users.noreply.github.com>
2021-05-10 23:45:59 +0530

    Solution to Question 1

--- C++/ProjectEuler/Question1.cpp
+++ C++/ProjectEuler/Question1.cpp
@@ -3,10 +3,23 @@
 
 int main(){
 
-    std::cout << "Test";
-    std::cout << "I don't know how this works!";
 
-    // Another test I guess.
+    // To find the sum of all numbers below n divisible by y, we can use the formula
+    // y*(n/y)((n/y) + 1)/2
+
+    int lim{1000};  // Given end point in the question
+    
+    int subtract{};
+    if(lim%3==0 || lim%5==0){
+
+        subtract = lim;
+
+    }
+
+
+    // We need to add the sum of numbers below n divisible by 3 and 5 respectively, and remove the repetitions
+    std::cout << 3*(lim/3)*((lim/3)+1)/2 + 5*(lim/5)*((lim/5)+1)/2 - 15*(lim/15)*((lim/15)+1)/2 - subtract;  
 
     return 0;
+
 }
